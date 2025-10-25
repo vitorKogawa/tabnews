9d6fdcb9ce1bf32a846b7fe7ba836e44511f2a56
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2025-10-25 19:52:48 -0300

    ajuste no arquivo de configuracao do next

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  // Opcional: Se seu projeto será hospedado em uma subpasta (ex: seu-usuario.github.io/seu-projeto)
-  // Certifique-se de substituir 'seu-repositorio' pelo nome do seu repositório.
-  basePath: process.env.NODE_ENV === 'production' ? '/tabnews' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/tabnews/' : '',
-  // Opcional: Desabilitar otimizações de imagem padrão se você tiver problemas
-  images: {
-    unoptimized: true,
-  },
-};
+// @ts-check
 
-module.exports = nextConfig;
+export default (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    output: "export",
+    // Opcional: Se seu projeto será hospedado em uma subpasta (ex: seu-usuario.github.io/seu-projeto)
+    // Certifique-se de substituir 'seu-repositorio' pelo nome do seu repositório.
+    basePath: process.env.NODE_ENV === "production" ? "/tabnews" : "",
+    assetPrefix: process.env.NODE_ENV === "production" ? "/tabnews/" : "",
+    // Opcional: Desabilitar otimizações de imagem padrão se você tiver problemas
+    images: {
+      unoptimized: true,
+    },
+  };
+  return nextConfig;
+};
