b695328668a9417d94f865ee16a061da4f909e37
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2026-01-16 12:21:24 -0300

    add teste de conexão com a base de dados

--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -4,3 +4,9 @@ test("GET to /api/v1/status return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
+
+test("Database connection must be online", async () => {
+  const resultExpect = 4;
+  const result = await database.query("SELECT 2 + 2 as sum;")
+  expect(result.rows[0].sum).toBe(resultExpect);
+})
