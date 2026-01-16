912706807e0c9011adade89394ded665432a2ef1
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2026-01-16 11:38:32 -0300

    importando arquivo de config db

--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,3 +1,5 @@
+const database = require('../../../../infra/database');
+
 test("GET to /api/v1/status return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
