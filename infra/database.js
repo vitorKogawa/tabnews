cafb108bbb59dc90f9e4ced5ab18428c42fb89eb
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2026-01-16 12:40:05 -0300

    removendo formatação desnecessária da variável de senha da base de dados

--- infra/database.js
+++ infra/database.js
@@ -6,9 +6,9 @@ async function query(queryObject) {
     const client = new Client({
       host: process.env.POSTGRES_HOST,
       user: process.env.POSTGRES_USER,
-      password: `${process.env.POSTGRES_PASSWORD}`,
+      password: process.env.POSTGRES_PASSWORD,
       port: process.env.POSTGRES_PORT,
-      database: process.env.POSTGRES_DATABASE,
+      database: process.env.POSTGRES_DB,
     });
     await client.connect();
     const result = await client.query(queryObject);
