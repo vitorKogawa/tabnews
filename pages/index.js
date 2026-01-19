0b84a5c29ecf04ce06920ae08b12634cc207095d
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2026-01-19 10:19:08 -0300

    ajustando componente `index.js` (homepage)

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() {
-  return (
-    <AnyText text="[Ambiente de Desenvolvimento] Bem-vindo ao clone do site TabNews!" />
-  );
+  return <h1>Clone do Tabnews!!!</h1>
 }
 
 export default Home;
